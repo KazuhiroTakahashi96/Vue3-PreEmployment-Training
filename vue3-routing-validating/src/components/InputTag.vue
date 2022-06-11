53e03c91885009b9c9dc00ae0389fdc6bbd78c1a
Kazu Takahashi <num96kazu19@icloud.com>
2022-06-12 00:54:57 +0900

    リポジトリー移行 作ったところまでをコピペした

---
+++ vue3-routing-validating/src/components/InputTag.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <label :for="props.name">{{ props.labelName }}</label>
+    <input v-model="value" :type="props.type" :name="props.name" />
+    <p>{{ errorMessage }}</p>
+  </div>
+</template>
+
+<!-- vee-validateについて
+https://reffect.co.jp/vue/veevaliate4
+ -->
+
+<script setup lang="ts">
+import { useField } from "vee-validate";
+import * as yup from "yup";
+
+const props = defineProps({
+  labelName: String,
+  type: String,
+  name: String,
+});
+
+const { errorMessage, value } = useField(
+  "fieldName",
+  yup.string().required().min(6)
+);
+</script>
+
+<style scoped>
+div {
+  margin-top: 50px;
+  height: 10vh;
+  text-align: center;
+}
+label {
+  margin-right: 20px;
+  color: #fff;
+}
+input {
+  width: 200px;
+  height: 30px;
+}
+p {
+  margin-top: 10px;
+  color: red;
+  font-size: 20px;
+}
+</style>
---
+++ vue3-routing-validating/src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<template>
+  <h2 class="title">Validation using Vee-Validate</h2>
+  <!-- <useForm> -->
+  <InputTag labelName="ユーザー名" type="text" name="name" />
+  <InputTag labelName="パスワード" type="password" name="password" />
+
+  <div class="btn">
+    <button type="submit">Submit</button>
+  </div>
+  <!-- </useForm> -->
+</template>
+
+<!-- vee-validateについて
+https://reffect.co.jp/vue/veevaliate4
+ -->
+<!-- <script setup>について -->
+<!-- https://zenn.dev/azukiazusa/articles/676d88675e4e74 -->
+<!-- https://tekrog.com/vue3-script-setup/ -->
+
+<script setup lang="ts">
+import { useForm } from "vee-validate";
+import InputTag from "../components/InputTag.vue";
+</script>
+
+<style scoped>
+h2 {
+  text-align: center;
+  color: aliceblue;
+}
+form {
+  margin: 50px 0;
+  font-size: 1.3rem;
+  text-align: center;
+}
+.btn {
+  text-align: center;
+}
+button {
+  width: 70px;
+  height: 25px;
+  margin-top: 50px;
+  font-size: 90%;
+}
+</style>
