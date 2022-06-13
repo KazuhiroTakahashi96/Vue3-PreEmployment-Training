8a29457a0b3e11be77459f7a3693ddbb6d9a646a
Kazu Takahashi <num96kazu19@icloud.com>
2022-06-13 17:49:33 +0900

    productItemコンポーネント作成

---
+++ vue3-routing-validating/src/components/products/productItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="item">
+    <img src="https://picsum.photos/200" alt="" />
+    <p>商品名 HOGE</p>
+    <p>値段 Hoge円</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  image: String,
+  name: String,
+  price: String,
+});
+</script>
+
+<style scoped>
+.item {
+  margin: 5px;
+}
+</style>
--- vue3-routing-validating/src/views/ProductView.vue
+++ vue3-routing-validating/src/views/ProductView.vue
@@ -1,19 +1,29 @@
 <template>
-  <div>This is a Product page.</div>
-  <ul>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-  </ul>
+  <div class="container">
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+    <ProductItem image="image" name="name" price="price" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ProductItem from "../components/products/productItem.vue";
+</script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 50px;
+}
+</style>
