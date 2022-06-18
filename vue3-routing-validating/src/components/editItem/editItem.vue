66c0d2ad168d08f7137809ef5fa4640cf0ced89b
Kazu Takahashi <num96kazu19@icloud.com>
2022-06-18 23:11:52 +0900

    在庫管理 削除機能の実装 要改善

--- vue3-routing-validating/src/components/editItem/editItem.vue
+++ vue3-routing-validating/src/components/editItem/editItem.vue
@@ -5,7 +5,9 @@
     <td>{{ props.itemPrice }}</td>
     <td>{{ props.itemDetail }}</td>
     <td>{{ props.itemStock }}</td>
-    <td><input type="button" value="削除" class="deleteBtn" /></td>
+    <td>
+      <input type="button" value="削除" class="deleteBtn" @click="deleteItem" />
+    </td>
   </tr>
 </template>
 
@@ -17,11 +19,64 @@ const props = defineProps({
   itemDetail: String,
   itemStock: Number,
 });
+
+async function fetchGraphQL(
+  operationsDoc: string,
+  operationName: string,
+  variables: object
+) {
+  const result = await fetch("http://localhost:8080/v1/graphql", {
+    method: "POST",
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables: variables,
+      operationName: operationName,
+    }),
+  });
+
+  return await result.json();
+}
+
+const operationsDoc = `
+  mutation insertItem($id: Int) {
+  delete_productitem(where: {id: {_eq: $id}}) {
+    affected_rows
+    returning {
+      id
+      name
+      price
+    }
+  }
+}
+`;
+
+function executeInsertItem() {
+  return fetchGraphQL(operationsDoc, "insertItem", {
+    id: props.itemId,
+  });
+}
+
+async function startExecuteInsertItem() {
+  const { errors, data } = await executeInsertItem();
+
+  if (errors) {
+    // handle those errors like a pro
+    console.error(errors);
+  }
+
+  // do something great with this precious data
+  console.log(data);
+}
+
+const deleteItem = () => {
+  startExecuteInsertItem();
+};
 </script>
 
 <style scoped>
 .deleteBtn {
   width: 50%;
   border-radius: 10px;
+  cursor: pointer;
 }
 </style>
